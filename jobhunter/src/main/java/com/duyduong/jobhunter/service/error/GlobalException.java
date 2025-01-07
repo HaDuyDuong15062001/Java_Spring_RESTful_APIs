cd4c8f170c8157157f160b025c69ce615cff1c53
Ha Duy Duong <haduyduong06031902@gmail.com>
2025-01-08 06:26:28 +0700

    error

--- jobhunter/src/main/java/com/duyduong/jobhunter/service/error/GlobalException.java
+++ jobhunter/src/main/java/com/duyduong/jobhunter/service/error/GlobalException.java
@@ -1,9 +1,11 @@
 package com.duyduong.jobhunter.service.error;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(value = IdInvalidException.class)
