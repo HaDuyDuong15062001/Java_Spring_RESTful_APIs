2472ceddc6a9d6defc3831b7b99bb21e0dbbd7a3
Ha Duy Duong <haduyduong06031902@gmail.com>
2025-01-08 15:31:40 +0700

    format rest response

--- jobhunter/src/main/java/com/duyduong/jobhunter/JobhunterApplication.java
+++ jobhunter/src/main/java/com/duyduong/jobhunter/JobhunterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobhunterApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JobhunterApplication.class, args);
 	}
 
--- jobhunter/src/main/java/com/duyduong/jobhunter/service/error/GlobalException.java
+++ jobhunter/src/main/java/com/duyduong/jobhunter/service/error/GlobalException.java
@@ -1,15 +1,21 @@
 package com.duyduong.jobhunter.service.error;
 
+import com.duyduong.jobhunter.domain.RestResponse;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Hidden
+//@Hidden
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(value = IdInvalidException.class)
     public ResponseEntity<String> handleIdException(IdInvalidException idInvalidException) {
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(idInvalidException.getMessage());
         return ResponseEntity.badRequest().body(idInvalidException.getMessage());
     }
 }
